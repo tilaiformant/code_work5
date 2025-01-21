fbeb06703edb79f32e28f5798b454d8d76bbd85d
wujianyu <wujianyu322@gmail.com>
2025-01-21 16:11:07 +0800

    計算陣列元素個數

--- test13.c
+++ test13.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a[5];
-    float b[5];
-    double c[5];
-    printf("a[5]陣列空間 = %d 位元組\n", sizeof(a));
-    printf("b[5]陣列空間 = %d 位元組\n", sizeof(b));
-    printf("c[5]陣列空間 = %d 位元組\n", sizeof(c));
+    int size;
+    int a[] = {1, 2, 3, 4};
     
+    size = sizeof(a) / sizeof(a[0]);
+    printf("陣列 a 的元素個數 = %d\n",size);
     getchar();
     return 0;
 
