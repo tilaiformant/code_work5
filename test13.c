531ac08ad35fdd32df525a42af7b8ad21725e5d6
wujianyu <wujianyu322@gmail.com>
2025-01-23 16:09:06 +0800

    計算成績平均用陣列呈現

--- test13.c
+++ test13.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 int main()
 {
-    int score[10];
+    int score[5], sum = 0;
     float ave;
-    int i,sum;
-    i = 0;
-    sum = 0;
-    printf("輸入 0 代表輸入結束\n");
-    
-    do
+
+    printf("輸入數學成績(0~100) : ");
+    scanf("%d",&score[0]);
+    printf("輸入英文分數(0~100) : ");
+    scanf("%d",&score[1]);
+    printf("輸入國文成績(0~100) : ");
+    scanf("%d",&score[2]);
+    printf("輸入物理分數(0~100) : ");
+    scanf("%d",&score[3]);
+    printf("輸入化學分數(0~100) : ");
+    scanf("%d",&score[4]);
+
+    for (int i = 0; i < 5; i++)
     {
-        printf("請輸入分數 ==> ");
-        scanf("%d",&score[i]);
         sum += score[i];
-    }  while (score[i++] > 0);
-        ave = (float) sum / ( i - 1);
-        printf("平均分數是 %6.2f \n",ave);
+    }
+    ave = (float)sum / 5;
+    printf("你的平均成績是: %.2f\n",ave);
+    
+    
     getchar();
     return 0;
 
