1ef8bff5ed4ce91c739e4bacf351c9190439e4a2
wujianyu <wujianyu322@gmail.com>
2025-01-27 11:40:30 +0800

    輸入a=4,b=5置換a=5,b=4

--- test13.c
+++ test13.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 int main()
-{  float high[7] = {30.0, 28.0, 19.0, 31.0, 33.0, 35.0, 32.0};
-   float low[7] = {20.0, 21.0, 19.0, 22.0, 23.0, 24.0, 20.0};
-   float avg[7];
-   float maxtem = high[0], mintem = low[0];
+{   int a;
+    int b;
+    int c;
+    printf("輸入 a 值 : ");
+    scanf("%d",&a);
+    printf("輸入 b 值 : ");
+    scanf("%d",&b);
 
-   for (int i = 0; i < 7; i++)
-   {
-    avg[i] = (high[i] + low[i]) / 2.0;
+    c = a;
+    a = b;
+    b = c;
+    printf("a = %d,b = %d\n",a,b);
 
-    if (high[i] > maxtem)
-    {
-        maxtem = high[i];
-    }
-
-    if (low[i] < mintem)
-    {
-        mintem = low[i];
-    }
-
-   } 
-    printf("最高溫 = %.1f\n",maxtem);
-    printf("最低溫 = %.1f\n",mintem);
-    printf("平均溫 = ");
-    for (int i = 0; i < 7; i++)
-    {
-        printf("%5.1f", avg[i]);
-    }
-    printf("\n");
     getchar();
     return 0;
 
