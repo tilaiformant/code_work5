16f7ba4c6d03321c691b95a288c2e6414ad8d097
wujianyu <wujianyu322@gmail.com>
2025-01-27 10:53:54 +0800

    找出平均溫度與最高溫最低溫

--- test13.c
+++ test13.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 int main()
-{   char *days[] = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
-    int tem[] = {25, 26, 28, 23, 24, 29, 27};
-    int mymax = tem[0];
-    int maxindex = 0;
+{  float high[7] = {30.0, 28.0, 19.0, 31.0, 33.0, 35.0, 32.0};
+   float low[7] = {20.0, 21.0, 19.0, 22.0, 23.0, 24.0, 20.0};
+   float avg[7];
+   float maxtem = high[0], mintem = low[0];
 
-    for (int i = 1; i < 7; i++ )
+   for (int i = 0; i < 7; i++)
+   {
+    avg[i] = (high[i] + low[i]) / 2.0;
+
+    if (high[i] > maxtem)
     {
-        if (tem[i] > mymax)
-        {
-            mymax = tem[i];
-            maxindex = i;
-        }
+        maxtem = high[i];
     }
-    
-    printf("最高溫是在%s, 溫度是%d度\n",days[maxindex], mymax);
+
+    if (low[i] < mintem)
+    {
+        mintem = low[i];
+    }
+
+   } 
+    printf("最高溫 = %.1f\n",maxtem);
+    printf("最低溫 = %.1f\n",mintem);
+    printf("平均溫 = ");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%5.1f", avg[i]);
+    }
+    printf("\n");
     getchar();
     return 0;
 
